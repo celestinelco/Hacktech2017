ab7a8d7d6e9769c00d72fc2d4e7276cafe1b668d
Celestine Co <celestine@Celestines-MacBook-Air.local>
2017-03-04 17:01:41 -0800

    Add project files

---
+++ Cal Poly Meal Plan/CalPolyMealPlan/CalPolyMealPlan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalPolyMealPlan
+//
+//  Created by Celestine Co on 3/4/17.
+//  Copyright © 2017 celestineco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
